253bee99358901e981bf551bb8f557c524938612
sereypsv <valid_@tut.by>
2024-06-12 22:15:11 +0300

    feat: adds styles to the modal window and some logic

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-const h1 = document.querySelector("h1");
-console.log(h1);
+const main = document.querySelector("main");
+const mainSection = main.querySelector(".main-section");
+const modalSection = main.querySelector(".modal-section");
+const overlay = main.querySelector(".overlay");
+
+const form = modalSection.querySelector("#login-form");
+
+const buttonStart = mainSection.querySelector(".button-start");
+const buttonCross = modalSection.querySelector(".cross");
+const buttonSubmit = modalSection.querySelector(".button-submit");
+
+buttonStart.onclick = () => {
+  overlay.style.display = "block";
+  modalSection.style.display = "block";
+};
+
+buttonCross.onclick = () => {
+  overlay.style.display = "none";
+  modalSection.style.display = "none";
+};
+
+overlay.onclick = (event) => {
+  if (event.target === overlay) {
+    overlay.style.display = "none";
+    modalSection.style.display = "none";
+  }
+};
+
+buttonSubmit.onclick = (event) => {
+  handleFormSubmit(event);
+  overlay.style.display = "none";
+  modalSection.style.display = "none";
+};
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const values = {};
+  for (const value of formData.entries()) {
+    values[value[0]] = value[1];
+  }
+  console.log(values);
+}
